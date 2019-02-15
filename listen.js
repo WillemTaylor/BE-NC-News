06a85fb9e69418b2aa34eefeb8adfabfcefe1933
WillemTaylor <willemtaylor@msn.com>
2019-02-15 16:59:13 +0000

    more changes

--- listen.js
+++ listen.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) console.log('ERROR', err);
-  else console.log(`server listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
