922c24441ec3eeb4f8763eba26aeec429c0b552d
WillemTaylor <willemtaylor@msn.com>
2019-03-12 17:13:26 +0000

    changes11

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,35 +2,31 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://cdn.pixabay.com/photo/2016/12/13/21/20/alien-1905155_960_720.png',
+    avatar_url: 'https://image.flaticon.com/icons/svg/188/188987.svg',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest/scale-to-width-down/250?cb=20170707233013',
+    avatar_url: 'https://image.flaticon.com/icons/svg/188/188918.svg',
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    avatar_url: 'https://image.flaticon.com/icons/svg/189/189001.svg',
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url:
-      'https://shop.mrmen.com/wp-content/themes/mrmen/assets/characterpic-square-mr%20messy.jpg',
+    avatar_url: 'https://image.flaticon.com/icons/svg/188/188990.svg',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://i.pinimg.com/originals/bb/31/fe/bb31feb091ece10cc8453dce2e1f6f12.jpg',
+    avatar_url: 'https://image.flaticon.com/icons/svg/188/188989.svg',
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url:
-      'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
+    avatar_url: 'https://image.flaticon.com/icons/svg/188/188993.svg',
   },
 ];
