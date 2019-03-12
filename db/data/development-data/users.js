da2749b6a681267979f1c288fedefb1870cc2aff
WillemTaylor <willemtaylor@msn.com>
2019-03-12 10:40:55 +0000

    more changes made from feedback2

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,7 +3,7 @@ module.exports = [
     username: 'tickle122',
     name: 'Tom Tickle',
     avatar_url:
-      'https://shop.mrmen.com/wp-content/uploads/2017/12/MRTICKLEPRINT_40c8d455-59ee-4446-9890-5a221ee3eb82.jpg',
+      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
   },
   {
     username: 'grumpy19',
